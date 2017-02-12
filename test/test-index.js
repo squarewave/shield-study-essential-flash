42bc3f62b5aa5fc97be916c1bee4ca9ffbc2d877
Doug Thayer <dougathayer@gmail.com>
2017-02-12 13:22:54 -0800

    fixed css on Windows and started writing tests

--- test/test-index.js
+++ test/test-index.js
@@ -1,4 +1,38 @@
-// TODO: I can't figure out a good way to test the actual
-// functionality of this addon using the normal add-on
-// testing tools. If someone has a suggestion that would be
-// very much appreciated.
+require("../lib/index");
+const { Cc, Ci } = require("chrome");
+
+const tabs = require("sdk/tabs");
+const { setTimeout } = require('sdk/timers');
+const { resolve } = require('sdk/fs/path');
+const { data } = require('sdk/self');
+const { viewFor } = require('sdk/view/core');
+const { browserWindows } = require('sdk/windows');
+
+const { startServerAsync } = require('./httpd');
+
+var srv = startServerAsync(2000);
+require("sdk/system/unload").when(function cleanup() {
+  srv.stop(function() { // you should continue execution from this point.
+  })
+});
+
+srv.registerPathHandler("/testFlashDoc", (req, res) => {
+  res.write(`
+<!DOCTYPE HTML>
+<html>
+<head>
+  <title>Test Essential Flash</title>
+</head>
+<h1>Testing...</h1>
+<body><object type="application/x-shockwave-flash"><embed src="not_here.swf"></object></body>
+</html>
+`);
+});
+
+exports['test main async'] = function(assert, done) {
+  assert.pass('async Unit test running!');
+  tabs.open("http://localhost:2000/testFlashDoc");
+  setTimeout(done, 500000);
+};
+
+require('sdk/test').run(exports);
